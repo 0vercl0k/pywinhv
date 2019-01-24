9fdaa0ae0d907381fa7ed0b1ae480b8f895370ae
0vercl0k <0vercl0k@tuxfamily.org>
2019-01-23 16:36:17 -0800

    Forward things in the library's namespace.

--- pywinhv/__init__.py
+++ pywinhv/__init__.py
@@ -1,2 +1,8 @@
 # Axel '0vercl0k' Souchet - 20 January 2019
+
+# Forward everything into the library's namespace.
 from pywinhv import *
+
+# Overwrite APIs with the higher level wrappers.
+from winhvplatform import *
+
